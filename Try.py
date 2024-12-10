3ed767cb46fef5e6087ddd6d12f29c275b8f4bbb
Wondee-Tesfa <habeshawwonde@gmail.com>
2024-12-10 08:47:20 -0800

    I modify it

--- Try.py
+++ Try.py
@@ -14,3 +14,16 @@ else:
         print(f"{number2} is the largest number.")
     else:
         print(f"{number3} is the largest number.")
+        
+#2 To calculate the area and curcumference of a CIRCLE by recieving the radius from the user ond using PYTHON Language.
+
+pi = 3.14
+rad = float(input("\nEnter the Radius : "))
+if rad >= 0:
+    area = pi*rad**2
+    circm= pi*rad*2
+    
+    print(f"The Area of radius {rad} is {area}M square. \n")
+    print(f"The Circumfference of radius {rad} is {circm}M.\n")
+else:
+    print("Your radius shoud be greater than 0.")    
